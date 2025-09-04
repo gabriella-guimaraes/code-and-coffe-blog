741b09b27acce8a1c889052203b962f7d919b2a2
gabriella-guimaraes <gabigara13@gmail.com>
2025-09-03 22:12:03 -0300

    feat(footer): update  do componente Footer com as cores do tema e link para o github

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 //Components
 import Box from "@/components/atoms/Box/Box";
 import Text from "@/components/atoms/Text/Text";
+import theme from './../../../theme/theme';
+import Link from "@/components/atoms/Link/Link";
 
 export default function Footer() {
     return(
-        <Box tag="footer" styleSheet={{backgroundColor: "blue"}}>
-            <Text>
-                Olha que rodapé lindão!
+        <Box tag="footer" styleSheet={{backgroundColor: theme.colors.neutral.x999, alignItems: 'center', justifyContent: 'center', padding: '16px', gap: '8px', display: 'flex', flexDirection: 'column'}}>
+            <Link href="https://github.com/gabriella-guimaraes" styleSheet={{  color: theme.colors.neutral.x000}}>
+                Feito com ❤️️ por Gabriella Guimarães
+            </Link>
+            <Text styleSheet={{  color: theme.colors.neutral.x900}}>
+                YHWH
             </Text>
         </Box>
     )
