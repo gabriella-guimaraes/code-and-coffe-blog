c395260731dfc27ce745df2eb5a6986cb24133bf
gabriella-guimaraes <gabigara13@gmail.com>
2025-09-07 15:18:32 -0300

    feat(Feed): layout base do componente Feed + responsividade

--- src/components/organisms/Feed/Feed.tsx
+++ src/components/organisms/Feed/Feed.tsx
@@ -3,6 +3,7 @@
 //Components
 import Box from "@/components/atoms/Box/Box";
 import Text from "@/components/atoms/Text/Text";
+import theme from "@/theme/theme";
 
 //React
 import React from "react";
@@ -13,11 +14,30 @@ interface FeedProps {
 
 export default function Feed({ children }: FeedProps) {
   return(
-    <Box tag="div" styleSheet={{}}>
-        <Text>
-            Feed Base
-        </Text>
-        {children}
+    <Box 
+      tag="div" 
+      styleSheet={{
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        tag="div"
+        styleSheet={{
+          backgroundColor: theme.colors.neutral.x999,
+          color: theme.colors.neutral.x000,
+          width: { xs: "100%", md: "60%" },
+          minHeight: "70vh",
+          borderRadius: "24px",
+          padding: "24px",
+        }}
+        >
+          <Text variant="heading1" styleSheet={{ marginBottom: "16px" }}>
+              Últimas postagens
+          </Text>
+          {children}
+      </Box>
     </Box>
   );
 }
